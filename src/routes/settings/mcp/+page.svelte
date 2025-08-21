2d8602182a2e62ee20643901fff0adcfdccfbc75
Gump <1989wg@gmail.com>
2025-08-21 16:19:54 +0800

    feat: Setting mcp edit style

--- src/routes/settings/mcp/+page.svelte
+++ src/routes/settings/mcp/+page.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
-    import Button from "$lib/components/ui/Button.svelte";
-  import StatusLabel from "$lib/components/ui/StatusLabel.svelte";
+  import McpServerTextEditModal from "$lib/components/settings/McpServerTextEditModal.svelte";
+  import Button from "$lib/components/ui/Button.svelte";
   import TableBaseRow from "$lib/components/ui/table/TableBaseRow.svelte";
   import TableGroup from "$lib/components/ui/table/TableGroup.svelte";
   import Toggle from "$lib/components/ui/Toggle.svelte";
-    import { ChevronDownIcon, ChevronsUpDown } from "@lucide/svelte";
+  import { ChevronsUpDown } from "@lucide/svelte";
+
+  let showMcpServerTextEditModal = $state(false);
 
   let expandedStates = $state<Record<string, boolean>>({});
 
@@ -37,7 +39,7 @@
         {
           name: "query",
           enabled: false,
-        }
+        },
       ],
     },
     {
@@ -64,10 +66,25 @@
     },
   ];
 
+  function handleManageMcp() {
+    showMcpServerTextEditModal = true;
+  }
 
-    function handleManageMcp(e: PointerEvent): void {
-        throw new Error("Function not implemented.");
+  async function handleConfirmMcpServerTextEdit(
+    event: CustomEvent<string>,
+  ) {
+    const mcpJson = event.detail;
+    try {
+      // await providerActions.createProvider(config);
+      showMcpServerTextEditModal = false;
+    } catch (error) {
+      console.error("Failed to create mcp server:", error);
     }
+  }
+
+  function handleCloseMcpServerTextEditModal() {
+    showMcpServerTextEditModal = false;
+  }
 </script>
 
 <div class="p-6 pr-8 flex flex-col gap-y-4">
@@ -76,26 +93,32 @@
       {#snippet toggleSnippet()}
         <Toggle checked={mcp.enabled} />
       {/snippet}
-      
+
       <TableBaseRow
         label={mcp.name}
         layout="vertical"
         rightContent={toggleSnippet}
       >
-        <div class="flex flex-col items-start gap-2 ">
-
-          <button onclick={() => expandedStates[mcp.name] = !expandedStates[mcp.name]} class="text-xs text-gray-500 hover:text-gray-700">
+        <div class="flex flex-col items-start gap-2">
+          <button
+            onclick={() =>
+              (expandedStates[mcp.name] = !expandedStates[mcp.name])}
+            class="text-xs text-gray-500 hover:text-gray-700"
+          >
             <div class="flex flex-row items-center gap-1">
               <span>tools</span>
               <ChevronsUpDown class="w-3 h-3" />
             </div>
-            
           </button>
-      
+
           {#if expandedStates[mcp.name]}
             <div class="flex flex-row items-center gap-2">
               {#each mcp.tools as tool}
-                <div class="flex flex-row items-center gap-1 bg-gray-200 rounded-sm py-0.5 px-1 text-xs">{tool.name}</div>
+                <div
+                  class="flex flex-row items-center gap-1 bg-gray-200 rounded-sm py-0.5 px-1 text-xs"
+                >
+                  {tool.name}
+                </div>
               {/each}
             </div>
           {/if}
@@ -110,3 +133,10 @@
     >
   </div>
 </div>
+
+{#if showMcpServerTextEditModal}
+  <McpServerTextEditModal
+    on:close={handleCloseMcpServerTextEditModal}
+    on:confirm={handleConfirmMcpServerTextEdit}
+  />
+{/if}
